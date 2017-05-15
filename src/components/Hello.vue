315bba44a2fa67bc41333cbf757920ac6d414cbe
xia <222@dd.com>
2017-05-15 16:29:55 +0800

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button v-on:click="click">add</button>
+    list>
+    <h2 v-for="it in list" v-bind="it"></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'chart',
+      list: []
+    }
+  },
+  methods: {
+    click: function () {
+      this.msg += '>'
+      this.list.push('aaa')
+    }
+  },
+  created: function () {
+    console.log('created....',this.msg)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
